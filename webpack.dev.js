2a54ef7a3ec833fb1bd44434b16c48623b14f113
maple <maple@nyan.lan>
2023-03-08 23:30:31 +0300

    test try

--- webpack.dev.js
+++ webpack.dev.js
@@ -58,7 +58,7 @@ module.exports = merge(common, {
     ]
   },
   optimization: {
-    minimize:true,
+    minimize:false,
     minimizer: [
       // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
 
